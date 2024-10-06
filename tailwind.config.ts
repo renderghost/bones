0ca08c9205b075077709dce866dbabaae5efcc5f
Barry Prendergast <barry.m.prendergast@gmail.com>
2024-10-06 12:13:27 +0200

    feat(core): add safer-buffer dependency and express package
    The changes in this commit include:
    
    1. Adding the `safer-buffer` package as a development dependency, which provides a safer alternative to the built-in Node.js `Buffer` class.
    
    2. Adding the `express` package as a development dependency, which is a popular web application framework for Node.js. This will be used for building a server-side application.
    
    The purpose of these changes is to enhance the project's functionality and security by incorporating these additional dependencies. The `safer-buffer` package helps mitigate potential security vulnerabilities, while the `express` package provides a powerful framework for building web applications.

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {},
+		colors: {
+			transparent: 'transparent',
+			attention: {
+				500: '#facc15',
+			},
+			brand: {
+				white: '#FFFFFF',
+				50: '#E5E8FF',
+				100: '#CCCFFF',
+				200: '#999EFF',
+				300: '#666BFF',
+				400: '#3336FF',
+				500: '#0000FF',
+				600: '#0004D6',
+				700: '#0006AD',
+				800: '#000785',
+				900: '#00024b',
+				950: '#000022',
+				black: '#000000',
+			},
+		},
+		fontFamily: {
+			sans: ['DM Sans', 'sans-serif'],
+			serif: ['DM Serif', 'serif'],
+		},
+	},
+	plugins: [],
+	darkMode: 'media',
+};
+
+export default config;
