98f5c62aef5fc8dc670541f081ae7bb024338f92
Barry Prendergast <barry.m.prendergast@gmail.com>
2024-10-06 14:35:14 +0200

    feat: Update dependencies in package-lock.json for shellwords and node-notifier
    feat: Update storybook addons and framework in main.ts
    
    feat: Update lit, lit-element, and lit-html dependencies in package-lock.json
    
    feat: Update @storybook/addon-a11y in package-lock.json

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,24 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
+	stories: [
+		'../stories/**/*.mdx',
+		'../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+	],
+	addons: [
+		'@chromatic-com/storybook',
+		'@storybook/addon-designs',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-links',
+		'@storybook/addon-onboarding',
+		'@storybook/addon-storysource',
+		'@storybook/addon-themes',
+		'@storybook/addon-viewport',
+	],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
 };
 export default config;
